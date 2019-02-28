ffb789d3cbe84c318efea2468a65db451591c6cf
SofiaFredriksson <sofiapkf@gmail.com>
2019-02-28 16:12:10 +0000

    Done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,38 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+    @dogs = Dog.all.sort{|left, right| left.employees.size <=> right.employees.size}.reverse
+  end
+
+  def adoptable
+    @adoptable = []
+    @dogs = Dog.all
+    @dogs.each do |dog|
+      if dog.employees.count == 0
+        @adoptable << dog
+      end
+    end
+    @adoptable
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def edit
+    @adoptable = Dog.find(params[:id])
+  end
+
+  def update
+    @adoptable = Dog.find(params[:id])
+    @adoptable.update(dog_params)
+    redirect_to @adoptable
+  end
+
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:employee_ids)
+  end
 end
