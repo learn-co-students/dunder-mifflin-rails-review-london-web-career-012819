7cedd56ae653efbff35f1dc728daa5b5991dd958
Alexander Dubois <sander.dubois@outlook.com>
2019-02-28 12:04:04 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :dogs
   resources :employees
+
+  get '/sorted_dogs', to: 'dogs#sorted'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
